33fa6a01e1b85b3cebf8cfad20d4ae801f3b65e8
Don Schenck <don.schenck@gmail.com>
2023-07-10 14:23:45 -0400

    Initial code load

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/albums")
+def getalbumlist():
+    album_list = [{
+        "albumID": 1,
+        "albumTitle": "Exile on Main Street",
+        "releaseYear": 1972
+    },
+    {
+        "albumID": 2,
+        "albumTitle": "Some Girls",
+        "releaseYear": 1978}
+    ]
+    return jsonify(album_list), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
